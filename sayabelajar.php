6e329a310ff180b73847cc1e49ea2e70e3d718b0
ahmadekahanggani2710 <79893062+ahmadekahanggani2710@users.noreply.github.com>
2021-03-06 14:32:10 +0700

    Add files via upload

---
+++ sayabelajar.php
@@ -0,0 +1,36 @@
+<?php
+
+    $txt = "Hello world! Saya Belajar";
+    $x = 5;
+    $y = 10.5;
+
+
+    echo $txt;
+    echo "<br>";
+    echo "Nama : Ahmad Eka hanggani";
+	 $x = 15;
+    $y = 20.5;
+
+
+    echo "<br>";
+	echo "Kelas: MI-P.18.1K";
+	 $x = 15;
+    $y = 20.5;
+
+
+    
+    echo "<br>";
+	echo "Tahun: 2018";
+	 $x = 15;
+    $y = 20.5;
+
+
+    
+    echo "<br>";
+	echo "Pemprograman Aplikasi Web";
+	 $x = 15;
+    $y = 20.5;
+
+    echo "<br>";
+
+?>
